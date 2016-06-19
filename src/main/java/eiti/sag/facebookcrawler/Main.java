168bce028cfe27f33dcd894f5c3b423676e17650
Adam Ciołkowski <adamciolkowski1@gmail.com>
2016-06-19 17:38:53 +0200

    Updated main

--- src/main/java/eiti/sag/facebookcrawler/Main.java
+++ src/main/java/eiti/sag/facebookcrawler/Main.java
@@ -2,9 +2,10 @@ package eiti.sag.facebookcrawler;
 
 import eiti.sag.facebookcrawler.accessor.FacebookAccessor;
 import eiti.sag.facebookcrawler.accessor.WebDriverFacebookAccessor;
-import eiti.sag.facebookcrawler.model.FacebookUser;
 import eiti.sag.facebookcrawler.repository.FacebookUserRepository;
 import eiti.sag.facebookcrawler.repository.json.JsonFacebookUserRepository;
+import eiti.sag.facebookcrawler.visitor.FacebookUserVisitor;
+import eiti.sag.facebookcrawler.visitor.SavingFacebookUserVisitor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,19 +15,21 @@ public class Main {
 
     public static void main(String[] args) {
         if(args.length != 2)
-            throw new IllegalArgumentException("No Facebook email and password given");
-        new Main().run(args[0], args[1]);
+            throw new IllegalArgumentException("No Facebook email, password or username given");
+        new Main().run(args[0], args[1], args[2]);
     }
 
-    private void run(String email, String password) {
+    private void run(String email, String password, String username) {
         WebDriver webDriver = new FirefoxDriver();
         FacebookAccessor accessor = new WebDriverFacebookAccessor(webDriver);
         accessor.login(email, password);
         File directory = new File(System.getProperty("user.home"), "facebook-users");
         FacebookUserRepository repository = new JsonFacebookUserRepository(directory);
 
-        FacebookUser user = accessor.fetchUser("zuck");
-        repository.save(user);
+        FacebookUserVisitor visitor = new SavingFacebookUserVisitor(repository);
+        FacebookCrawler crawler = new FacebookCrawler(accessor, visitor);
+        crawler.loadRecursively(username);
+
         accessor.logout();
     }
 
