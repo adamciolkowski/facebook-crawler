4d0697517f865252d4536b7b33bb7538786017eb
Adam Ciołkowski <adamciolkowski1@gmail.com>
2016-07-16 22:47:08 +0200

    Extract experience

--- src/main/java/eiti/sag/facebookcrawler/accessor/jsoup/JsoupFacebookAccessor.java
+++ src/main/java/eiti/sag/facebookcrawler/accessor/jsoup/JsoupFacebookAccessor.java
@@ -1,11 +1,15 @@
 package eiti.sag.facebookcrawler.accessor.jsoup;
 
 import eiti.sag.facebookcrawler.accessor.FacebookAccessor;
+import eiti.sag.facebookcrawler.model.Education;
+import eiti.sag.facebookcrawler.model.Experience;
 import eiti.sag.facebookcrawler.model.FacebookUser;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -40,23 +44,57 @@ public class JsoupFacebookAccessor implements FacebookAccessor {
 
     private FacebookUser doFetchUser(String username) throws IOException {
         Document aboutPage = getPage(username, "about");
+        Document educationPage = getPage(username, "about?section=education");
         Document friendsPage = getPage(username, "friends_all");
-        return fetchUser(aboutPage, friendsPage);
+        return fetchUser(aboutPage, educationPage, friendsPage);
     }
 
     private Document getPage(String username, String section) throws IOException {
         return Jsoup.parse(htmlPageGetter.get(BASE_URL + username + "/" + section));
     }
 
-    private FacebookUser fetchUser(Document aboutPage, Document friendsPage) {
+    private FacebookUser fetchUser(Document aboutPage, Document educationPage, Document friendsPage) {
         FacebookUser user = new FacebookUser();
         user.setName(extractName(aboutPage));
         user.setFriendsIds(fetchFriendsIds(friendsPage));
+        user.setExperience(extractExperience(educationPage));
         return user;
     }
 
     private String extractName(Document document) {
         return document.getElementById("fb-timeline-cover-name").text();
+    }
+
+    private Experience extractExperience(Document educationPage) {
+        List<Education> works = extractEducation(educationPage);
+        return new Experience(null, works);
+    }
+
+    private List<Education> extractEducation(Document educationPage) {
+        String cssQuery = "div[data-pnref='edu'] " +
+                "ul[class='uiList fbProfileEditExperiences _4kg _4ks'] > li";
+        Elements elements = educationPage.select(cssQuery);
+        return extractEducation(elements);
+    }
+
+    private List<Education> extractEducation(Elements elements) {
+        return elements.stream()
+                .map(this::extractEducation)
+                .collect(toList());
+    }
+
+    private Education extractEducation(Element element) {
+        Element a = element.select("div[class='_2lzr _50f5 _50f7'] a").get(0);
+        String name = a.text();
+        String link = a.attr("href");
+        return new Education(name, extractInfo(element), link);
+    }
+
+    private String extractInfo(Element e) {
+        Elements elements = e.select("div[class='fsm fwn fcg']");
+        if(elements.isEmpty())
+            return null;
+        return elements.get(0).text();
     }
 
     private List<String> fetchFriendsIds(Document document) {
