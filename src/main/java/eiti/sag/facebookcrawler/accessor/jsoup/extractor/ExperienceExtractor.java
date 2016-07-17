5fcbb88649de62f0cf8eb05b2482f51e4bd14951
Adam Ciołkowski <adamciolkowski1@gmail.com>
2016-07-17 13:26:30 +0200

    Extract works

--- src/main/java/eiti/sag/facebookcrawler/accessor/jsoup/extractor/ExperienceExtractor.java
+++ src/main/java/eiti/sag/facebookcrawler/accessor/jsoup/extractor/ExperienceExtractor.java
@@ -2,6 +2,7 @@ package eiti.sag.facebookcrawler.accessor.jsoup.extractor;
 
 import eiti.sag.facebookcrawler.model.Education;
 import eiti.sag.facebookcrawler.model.Experience;
+import eiti.sag.facebookcrawler.model.Work;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -15,7 +16,8 @@ public class ExperienceExtractor implements DocumentExtractor<Experience> {
     @Override
     public Experience extract(Document document) {
         List<Education> education = extractEducation(document);
-        return new Experience(null, education);
+        List<Work> works = extractWorks(document);
+        return new Experience(works, education);
     }
 
     private List<Education> extractEducation(Document page) {
@@ -42,6 +44,26 @@ public class ExperienceExtractor implements DocumentExtractor<Experience> {
         if(elements.isEmpty())
             return null;
         return elements.get(0).text();
+    }
+
+    private List<Work> extractWorks(Document page) {
+        String cssQuery = "div[data-pnref='work'] " +
+                "ul[class='uiList fbProfileEditExperiences _4kg _4ks'] > li";
+        Elements elements = page.select(cssQuery);
+        return extractWorks(elements);
+    }
+
+    private List<Work> extractWorks(Elements elements) {
+        return elements.stream()
+                .map(this::extractWork)
+                .collect(toList());
+    }
+
+    private Work extractWork(Element element) {
+        Element a = element.select("div[class='_2lzr _50f5 _50f7'] a").get(0);
+        String name = a.text();
+        String link = a.attr("href");
+        return new Work(name, extractInfo(element), link);
     }
 
 }
